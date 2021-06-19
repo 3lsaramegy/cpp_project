bade9f6b9a2f2ae3a89766a71eccbc9ec1bfd2f3
3lsaramegy <mahmoudelsaramegy@gmail.com>
2021-06-19 19:06:49 +0200

    new taskies

---
+++ arrey.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std ;
+
+int main(){
+int size ,size2 ;
+string frist_name[5];
+string scande_name[5];
+string famliy_name[5];
+
+
+
+//________________________________
+frist_name[0]="Donia";
+scande_name[0]="Ashraf";
+famliy_name[0]="nono";
+//_________________________________
+
+cout<<"Enter code name\n";
+cin>>size;
+cout<<"Name:"<<frist_name[size]<<" "<<scande_name[size]<<"\n";
+}
